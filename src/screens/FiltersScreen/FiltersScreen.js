3cf0b429f02ffda56bc2a361a3009f320d7c72c4
NeRo8 <artur.duchenko@gmail.com>
2019-10-15 12:08:35 +0300

    Work with filters screen

--- src/screens/FiltersScreen/FiltersScreen.js
+++ src/screens/FiltersScreen/FiltersScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Input } from 'react-native-elements';
+
+import styles from './styles';
 
 class FiltersScreen extends Component {
   constructor(props) {
@@ -7,7 +10,78 @@ class FiltersScreen extends Component {
     this.state = {};
   }
   render() {
-    return <View style={{ flex: 1 }} />;
+    return (
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={styles.container}>
+          <View style={styles.filterElement}>
+            <Text style={styles.textBlock}>TYPE OF AD</Text>
+            <View style={styles.selectBlock}>
+              <View style={styles.element}>
+                <Text style={styles.textElement}>Private</Text>
+              </View>
+              <View style={styles.elementActive}>
+                <Text style={styles.textElementActive}>Buisnes</Text>
+              </View>
+              <View style={styles.element}>
+                <Text style={styles.textElement}>All</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.filterElement}>
+            <Text style={styles.textBlock}>STATE OF PRODUCT</Text>
+            <View style={styles.selectBlock}>
+              <View style={styles.element}>
+                <Text style={styles.textElement}>New</Text>
+              </View>
+              <View style={styles.elementActive}>
+                <Text style={styles.textElementActive}>b/a</Text>
+              </View>
+              <View style={styles.element}>
+                <Text style={styles.textElement}>All</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.filterElement}>
+            <Text style={styles.textBlock}>Price</Text>
+            <View>
+              <View style={styles.blockPrice}>
+                <Input
+                  placeholder="0"
+                  label="From"
+                  labelStyle={styles.labelInput}
+                  inputContainerStyle={styles.inputStyle}
+                  containerStyle={styles.inputContainer}
+                />
+                <View style={{ width: 10 }} />
+                <Input
+                  placeholder="0"
+                  label="To"
+                  labelStyle={styles.labelInput}
+                  inputContainerStyle={styles.inputStyle}
+                  containerStyle={styles.inputContainer}
+                />
+              </View>
+            </View>
+          </View>
+          <View style={styles.filterElement}>
+            <Text style={styles.textBlock}>CHOOSE CATEGORY</Text>
+            <ScrollView
+              horizontal
+              contentContainerStyle={styles.blockPrice}
+              showsHorizontalScrollIndicator={false}>
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+              <View style={styles.categoryElement} />
+            </ScrollView>
+          </View>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
---
+++ src/screens/FiltersScreen/styles.js
@@ -0,0 +1,67 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from '../../constants/colors';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  filterElement: {
+    marginVertical: 20,
+  },
+  textBlock: {
+    fontWeight: 'bold',
+  },
+  selectBlock: {
+    marginTop: 10,
+    flexDirection: 'row',
+    height: 40,
+  },
+  element: {
+    flex: 1,
+    borderColor: 'silver',
+    borderWidth: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textElement: {},
+  elementActive: {
+    flex: 1,
+    borderColor: colors.HEADER,
+    borderWidth: 0.5,
+    backgroundColor: colors.HEADER,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textElementActive: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  blockPrice: {
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  labelInput: {
+    padding: 0,
+    marginBottom: 5,
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 0,
+  },
+  inputStyle: {
+    paddingHorizontal: 10,
+    borderWidth: 0.5,
+    borderColor: 'silver',
+    //borderBottomWidth: 0,
+  },
+  categoryElement: {
+    marginHorizontal: 10,
+    height: 100,
+    width: 100,
+    backgroundColor: 'silver',
+  },
+});
