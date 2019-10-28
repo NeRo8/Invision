01c615cc584c4992a6e607d82df85e3ad487df41
NeRo8 <artur.duchenko@gmail.com>
2019-10-28 18:58:34 +0200

    Add modal screen

--- src/screens/Auth/ForgotPasswordScreen/ForgotPasswordScreen.js
+++ src/screens/Auth/ForgotPasswordScreen/ForgotPasswordScreen.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  StatusBar,
+} from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import globalStyles from '../../../constants/globalStyles';
+import ModalSuccess from './ModalSuccess';
 
 class ForgotPasswordScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showModal: false,
+    };
   }
+
+  handlePressShowModal = () => {
+    this.setState({
+      showModal: !this.state.showModal,
+    });
+  };
+
   render() {
     return (
       <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <StatusBar translucent barStyle="light-content" />
         <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
           <View style={{ paddingHorizontal: 30 }}>
             <Text style={[globalStyles.gothamBook, { fontSize: 24 }]}>
@@ -24,10 +41,13 @@ class ForgotPasswordScreen extends Component {
               Enter your email and we will send you access
             </Text>
             <Input
-              placeholder="Password"
+              label="Password*"
+              labelStyle={[
+                globalStyles.gothamBook,
+                { fontSize: 17, fontWeight: '100' },
+              ]}
               inputStyle={[globalStyles.gothamBook, { fontSize: 17 }]}
-              inputContainerStyle={{ marginTop: 70 }}
-              containerStyle={{ paddingHorizontal: 0 }}
+              containerStyle={{ paddingHorizontal: 0, marginTop: 70 }}
             />
             <Button
               title="Send password"
@@ -37,8 +57,13 @@ class ForgotPasswordScreen extends Component {
               ]}
               buttonStyle={{ height: 50 }}
               containerStyle={{ marginTop: 35 }}
+              onPress={() => this.handlePressShowModal()}
             />
           </View>
+          <ModalSuccess
+            show={this.state.showModal}
+            onPressSubmit={this.handlePressShowModal}
+          />
         </SafeAreaView>
       </KeyboardAvoidingView>
     );
