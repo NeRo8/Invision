f6f208f3c81a6a7d27dc583307df1bcc23d6e3ce
NeRo8 <artur.duchenko@gmail.com>
2019-11-02 15:50:52 +0200

    Add grayscale to Profile Screen

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -2,89 +2,161 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  TouchableOpacity,
   FlatList,
   ImageBackground,
   ScrollView,
-  Image,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Icon, Button } from 'react-native-elements';
+import { Grayscale } from 'react-native-color-matrix-image-filters';
+
+import ModalProfile from '../../components/ModalProfile';
+import TextPicker from '../../components/TextPicker';
 
 import globalStyles from '../../constants/globalStyles';
 import { colors } from '../../constants/colors';
 
 import styles from './styles';
-import ModalProfile from '../../components/ModalProfile/ModalProfile';
 
-const ElementFL = ({ item, showOption }) => (
-  <ImageBackground
-    style={styles.elementContainer}
-    source={require('../../assets/images/background-phone.jpg')}>
-    <View style={styles.container}>
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <View
-          style={{
-            marginLeft: 40,
-            flex: 1,
-            paddingTop: 25,
-          }}>
-          <Text
-            style={[globalStyles.gothamMediumRegular, styles.fontDescription]}>
-            Whole every miles as tiled at seven or. Wished he entire esteem mr
-            oh by.
-          </Text>
+const ElementFL = ({ item, showOption, grayscale }) => {
+  if (grayscale) {
+    return (
+      <Grayscale style={{ flex: 1 }}>
+        <ImageBackground
+          style={styles.elementContainer}
+          source={require('../../assets/images/background-phone.jpg')}>
+          <View style={styles.container}>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              <View
+                style={{
+                  marginLeft: 40,
+                  flex: 1,
+                  paddingTop: 25,
+                }}>
+                <Text
+                  style={[
+                    globalStyles.gothamMediumRegular,
+                    styles.fontDescription,
+                  ]}>
+                  Whole every miles as tiled at seven or. Wished he entire
+                  esteem mr oh by.
+                </Text>
+              </View>
+              <View
+                style={{
+                  marginRight: 25,
+                  marginTop: 36,
+                  marginLeft: 25,
+                }}>
+                <Icon
+                  name="dots-vertical"
+                  type="material-community"
+                  containerStyle={styles.iconDots}
+                  onPress={() => showOption(true)}
+                />
+              </View>
+            </View>
+            <View
+              style={{
+                backgroundColor: 'white',
+                width: '100%',
+                height: 44,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+              }}>
+              <Text style={[globalStyles.gothamBook]}>$150</Text>
+              <Text style={[globalStyles.gothamBook]}>01.03.05</Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={[globalStyles.gothamBook]}>10</Text>
+                <Icon
+                  name="ios-mail"
+                  type="ionicon"
+                  color="silver"
+                  containerStyle={{ marginLeft: 5 }}
+                />
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </Grayscale>
+    );
+  }
+  return (
+    <ImageBackground
+      style={styles.elementContainer}
+      source={require('../../assets/images/background-phone.jpg')}>
+      <View style={styles.container}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View
+            style={{
+              marginLeft: 40,
+              flex: 1,
+              paddingTop: 25,
+            }}>
+            <Text
+              style={[
+                globalStyles.gothamMediumRegular,
+                styles.fontDescription,
+              ]}>
+              Whole every miles as tiled at seven or. Wished he entire esteem mr
+              oh by.
+            </Text>
+          </View>
+          <View
+            style={{
+              marginRight: 25,
+              marginTop: 36,
+              marginLeft: 25,
+            }}>
+            <Icon
+              name="dots-vertical"
+              type="material-community"
+              containerStyle={styles.iconDots}
+              onPress={() => showOption(true)}
+            />
+          </View>
         </View>
         <View
           style={{
-            marginRight: 25,
-            marginTop: 36,
-            marginLeft: 25,
-          }}>
-          <Icon
-            name="dots-vertical"
-            type="material-community"
-            containerStyle={styles.iconDots}
-            onPress={() => showOption(true)}
-          />
-        </View>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'white',
-          width: '100%',
-          height: 44,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-        }}>
-        <Text style={[globalStyles.gothamBook]}>$150</Text>
-        <Text style={[globalStyles.gothamBook]}>01.03.05</Text>
-        <View
-          style={{
+            backgroundColor: 'white',
+            width: '100%',
+            height: 44,
             flexDirection: 'row',
             alignItems: 'center',
-            justifyContent: 'center',
+            justifyContent: 'space-around',
           }}>
-          <Text style={[globalStyles.gothamBook]}>10</Text>
-          <Icon
-            name="ios-mail"
-            type="ionicon"
-            color="silver"
-            containerStyle={{ marginLeft: 5 }}
-          />
+          <Text style={[globalStyles.gothamBook]}>$150</Text>
+          <Text style={[globalStyles.gothamBook]}>01.03.05</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={[globalStyles.gothamBook]}>10</Text>
+            <Icon
+              name="ios-mail"
+              type="ionicon"
+              color="silver"
+              containerStyle={{ marginLeft: 5 }}
+            />
+          </View>
         </View>
       </View>
-    </View>
-  </ImageBackground>
-);
+    </ImageBackground>
+  );
+};
 
 class ProfileScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: true,
-      inactive: false,
       data: [
         {
           id: 1,
@@ -128,6 +200,25 @@ class ProfileScreen extends Component {
         },
       ],
       showOption: false,
+      filters: [
+        {
+          id: 0,
+          title: 'Active',
+          active: true,
+          func: () => {
+            this.setState({ grayscale: false });
+          },
+        },
+        {
+          id: 1,
+          title: 'Unactive',
+          active: false,
+          func: () => {
+            this.setState({ grayscale: true });
+          },
+        },
+      ],
+      grayscale: false,
     };
   }
 
@@ -145,9 +236,18 @@ class ProfileScreen extends Component {
     });
   };
 
-  render() {
-    const { active, inactive } = this.state;
+  handlePressFilter = id => {
+    const { filters } = this.state;
+    const newFilter = filters.map(item =>
+      item.id === id ? { ...item, active: true } : { ...item, active: false },
+    );
 
+    this.setState({
+      filters: newFilter,
+    });
+  };
+
+  render() {
     return (
       <ScrollView style={styles.container}>
         <LinearGradient
@@ -260,46 +360,20 @@ class ProfileScreen extends Component {
           </Text>
 
           <View style={styles.selectedContainer}>
-            <TouchableOpacity
-              style={
-                active ? styles.selectedElementActive : styles.selectedElement
-              }
-              onPress={() => this.handlePressSelectedView('active')}>
-              <Text
-                style={
-                  active
-                    ? [
-                        globalStyles.gothamBold,
-                        styles.selectedTextElementActive,
-                      ]
-                    : [globalStyles.gothamBook, styles.selectedTextElement]
-                }>
-                Active
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={
-                inactive ? styles.selectedElementActive : styles.selectedElement
-              }
-              onPress={() => this.handlePressSelectedView('inactive')}>
-              <Text
-                style={
-                  inactive
-                    ? [
-                        globalStyles.gothamBold,
-                        styles.selectedTextElementActive,
-                      ]
-                    : [globalStyles.gothamBook, styles.selectedTextElement]
-                }>
-                Inactive
-              </Text>
-            </TouchableOpacity>
+            <TextPicker
+              dataList={this.state.filters}
+              onPressElement={this.handlePressFilter}
+            />
           </View>
           <View style={[styles.container, { marginTop: 10 }]}>
             <FlatList
               data={this.state.data}
               renderItem={({ item }) => (
-                <ElementFL item={item} showOption={this.showOption} />
+                <ElementFL
+                  item={item}
+                  showOption={this.showOption}
+                  grayscale={this.state.grayscale}
+                />
               )}
             />
           </View>
