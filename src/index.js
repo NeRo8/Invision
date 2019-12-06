1c029e049888c65a148891fe40c3eefd80bef1d9
nero <artur.duchenko@gmail.com>
2019-12-06 16:53:35 +0200

    Add redux persist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import AppNavigation from './navigation';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 const App = () => (
   <Provider store={store}>
-    <AppNavigation />
+    <PersistGate
+      loading={<ActivityIndicator size="large" color="blue" />}
+      persistor={persistor}>
+      <AppNavigation />
+    </PersistGate>
   </Provider>
 );
 
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,22 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import authReducer from '../reducers/Auth';
 import adsReducer from '../reducers/adsReducer';
 import inKuwaitReducer from '../reducers/inKuwait';
 import filtersReducer from '../reducers/filtersReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+  whitelist: ['auth'],
+  blacklist: ['ads', 'inKuwait', 'filters'],
+};
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -14,6 +25,7 @@ const rootReducer = combineReducers({
   filters: filtersReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const pReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+export const store = createStore(pReducer, applyMiddleware(thunk, logger));
+export const persistor = persistStore(store);
